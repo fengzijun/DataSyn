54299f24e8d965a23134621f03e554eb026da21a
fengzijun <fengzijun_2005@163.com>
2017-12-12 17:52:41 +0800

    updaste service

---
+++ DayaSynConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayaSynConsole
+{
+    class Program
+    {
+        static string conenction = System.Configuration.ConfigurationManager.AppSettings["connection"];
+        static string conenction2 = System.Configuration.ConfigurationManager.AppSettings["connection"];
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        static void GetData()
+        {
+
+        }
+    }
+}
